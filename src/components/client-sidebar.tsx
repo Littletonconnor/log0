f441abea31e94771721eef08a3fd057d2c410c80
Connor Littleton <connorlittleton@wealthfront.com>
2025-05-19 08:48:12 -0700

    More fun

--- src/components/client-sidebar.tsx
+++ src/components/client-sidebar.tsx
@@ -1,24 +1,32 @@
+"use client";
+
 import { TooltipProvider } from "@radix-ui/react-tooltip";
+import { FolderOpen, LayoutGrid, PanelRight } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { GlobalSearch } from "./global-search";
+import { Log0Logo } from "./icons/log0-logo";
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
   SidebarHeader,
+  SidebarMenu,
   SidebarMenuButton,
+  SidebarMenuItem,
   SidebarTrigger,
 } from "./ui/sidebar";
-import Link from "next/link";
-import { Log0Logo } from "./icons/log0-logo";
-import { PanelRight } from "lucide-react";
-import { GlobalSearch } from "./global-search";
 
 export function ClientSidebar() {
+  const pathname = usePathname();
+
   return (
     <TooltipProvider delayDuration={0}>
       <Sidebar
         collapsible="icon"
         className="relative flex h-full flex-col border border-r text-foreground"
       >
+        {/* Sidebar header */}
         <SidebarHeader className="flex w-full flex-row justify-between group-data-[collapsible=icon]:flex-col">
           <div className="flex items-center gap-2">
             <Link href="/" className="flex items-center gap-2">
@@ -40,9 +48,45 @@ export function ClientSidebar() {
             </SidebarTrigger>
           </SidebarMenuButton>
         </SidebarHeader>
+
+        {/* Sidebar content */}
         <SidebarContent>
+          {/* Global search command menu */}
           <SidebarGroup className="relative">
             <GlobalSearch />
+          </SidebarGroup>
+
+          <SidebarGroup>
+            <SidebarMenu>
+              {/* Dashboard link */}
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild tooltip="Home">
+                  <Link
+                    href="/dashboard"
+                    data-active={pathname === "/dashboard"}
+                    className="flex items-center gap-2 text-muted-foreground group-data-[collapsible=icon]:justify-center"
+                  >
+                    <LayoutGrid className="w-4 h-4 shrink-0" />
+                    <span className="truncate max-w-28 group-data-[collapsible=icon]:hidden">
+                      Dashboard
+                    </span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+
+              {/* Documents section */}
+              <SidebarMenuItem>
+                <SidebarMenuButton className="flex w-full items-center">
+                  <FolderOpen />
+                  <span>My documents</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+
+              {/* Resources section */}
+              <SidebarMenuItem></SidebarMenuItem>
+
+              {/* Log-in/log-out buttons */}
+            </SidebarMenu>
           </SidebarGroup>
         </SidebarContent>
       </Sidebar>
